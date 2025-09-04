1a50b76984f5b0aa6afb5e934d35c5183aa98255
GuirinVeronica <116305319+GuirinV@users.noreply.github.com>
2025-09-03 21:10:27 -0300

    TPractico1/

---
+++ TPractico1/HolaMundoProject/src/DatosPersonales.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author veron
+ */
+public class DatosPersonales {
+    public static void main(String[] args) {
+        // Declaración e inicialización de variables
+        String nombre = "Juan"; // nombre
+        int edad = 25;          // edad
+        double altura = 1.75;   // altura en metros
+        boolean estudiante = true; // se cambia true a false si no es estudiante
+
+        // Imprimir los valores en pantalla
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+        System.out.println("Estudiante: " + estudiante);
+    }
+}
